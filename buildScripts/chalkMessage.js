838c2abe9f28b1b80026fbd5504c1a773138e2f5
Marcelo Barsotti <mbarsott@gmail.com>
2019-10-26 21:46:31 -0400

    9. Linting

---
+++ buildScripts/chalkMessage.js
@@ -0,0 +1,3 @@
+import chalk from "chalk";
+
+console.log(chalk.yellow("Starting chalkMessage.js...")); // eslint-disable-line no-console
--- buildScripts/task-1.js
+++
@@ -1,5 +0,0 @@
-import chalk from "chalk";
-
-console.log(
-  chalk.yellow("Starting task-1 in parallel (could be Localtunnel)...")
-);
